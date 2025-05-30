554c0590a261234f1f728e131318c9603804f66a
Saad Shoaib <binshoaibsaad@gmail.com>
2025-05-31 02:23:42 +0500

    api url updated

--- virtual-arena-user/src/utils/ApiUrl.js
+++ virtual-arena-user/src/utils/ApiUrl.js
@@ -1,23 +1,18 @@
-// import { useSelector } from "react-redux";
+// src/constants.js or wherever this file is
 
-// export const API_URL = 'https://virtual-arena-backend.vercel.app/api/v1';
-export const API_URL = 'http://localhost:8080/api/v1';
-export const SOCKET_URL = 'http://localhost:5000/';
-// export const useAuthToken = () => {
-//     return useSelector((state) => state.login.token);
-// };
-
+export const API_URL = 'http://69.62.69.119:8080/api/v1';
+export const SOCKET_URL = 'http://69.62.69.119:5000/';
 
 export const getAuthHeaders = () => {
-    const authToken = localStorage.getItem('token');
-    
-    if (!authToken) {
-      throw new Error('No auth token found');
-    }
+  const authToken = localStorage.getItem('token');
   
-    return {
-      headers: {
-        Authorization: `Bearer ${authToken}`
-      }
-    };
+  if (!authToken) {
+    throw new Error('No auth token found');
+  }
+
+  return {
+    headers: {
+      Authorization: `Bearer ${authToken}`
+    }
   };
+};
