bddcede72f883ad066bb501fb9a5f5bf5d8edded
Saad Shoaib <binshoaibsaad@gmail.com>
2025-05-31 02:28:18 +0500

    admin url updated

--- virtual-arena-admin/src/utils/ApiUrl.js
+++ virtual-arena-admin/src/utils/ApiUrl.js
@@ -1,39 +1,37 @@
 // import { useSelector } from "react-redux";
 
 // export const API_URL = 'https://virtual-arena-backend.vercel.app/api/v1';
-export const API_URL = 'http://localhost:8080/api/v1';
-export const SOCKET_URL = 'http://localhost:5000/';
+export const API_URL = 'http://69.62.69.119:8080/api/v1';
+export const SOCKET_URL = 'http://69.62.69.119:5000/';
 // export const useAuthToken = () => {
 //     return useSelector((state) => state.login.token);
 // };
 
-
 export const getAuthHeaders = () => {
-    const authToken = localStorage.getItem('token');
-    
-    if (!authToken) {
-      throw new Error('No auth token found');
-    }
-  
-    return {
-      headers: {
-        Authorization: `Bearer ${authToken}`
-      }
-    };
-  };
+  const authToken = localStorage.getItem('token');
 
+  if (!authToken) {
+    throw new Error('No auth token found');
+  }
 
-  export const getFormDataAuthHeaders = () => {
-    const authToken = localStorage.getItem('token');
-    
-    if (!authToken) {
-      throw new Error('No auth token found');
+  return {
+    headers: {
+      Authorization: `Bearer ${authToken}`
     }
-  
-    return {
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-        "Content-Type": "multipart/form-data",
-      }
-    };
   };
+};
+
+export const getFormDataAuthHeaders = () => {
+  const authToken = localStorage.getItem('token');
+
+  if (!authToken) {
+    throw new Error('No auth token found');
+  }
+
+  return {
+    headers: {
+      Authorization: `Bearer ${authToken}`,
+      "Content-Type": "multipart/form-data",
+    }
+  };
+};
