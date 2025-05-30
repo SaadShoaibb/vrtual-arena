2f22adc37f18f888512d51efd452818b48f300f0
Saad Shoaib <binshoaibsaad@gmail.com>
2025-05-31 03:32:56 +0500

    bookings updated

--- virtual-arena-admin/src/utils/pusher.js
+++ virtual-arena-admin/src/utils/pusher.js
@@ -1,8 +1,9 @@
+// utils/pusher.js
 import Pusher from 'pusher-js';
 
-const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, {
-  cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
-  forceTLS: true
-});
-
-export default pusher;
+export const getPusherInstance = () => {
+  return new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, {
+    cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
+    forceTLS: true,
+  });
+};
