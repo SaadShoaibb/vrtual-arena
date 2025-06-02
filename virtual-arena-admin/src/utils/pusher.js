f6590082808398027e12d72814d5644766a4f4c6
Saad Shoaib <binshoaibsaad@gmail.com>
2025-06-03 03:18:46 +0500

    pusher patch

--- virtual-arena-admin/src/utils/pusher.js
+++ virtual-arena-admin/src/utils/pusher.js
@@ -1,9 +1,8 @@
-// utils/pusher.js
 import Pusher from 'pusher-js';
 
-export const getPusherInstance = () => {
-  return new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, {
-    cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
-    forceTLS: true,
-  });
-};
+const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, {
+  cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
+  forceTLS: true
+});
+
+export default pusher;
