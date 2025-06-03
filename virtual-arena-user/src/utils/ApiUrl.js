e50ee16b89253438a784c59d8914768822855a6c
Saad Shoaib <binshoaibsaad@gmail.com>
2025-06-04 00:27:53 +0500

    revert code

--- virtual-arena-user/src/utils/ApiUrl.js
+++ virtual-arena-user/src/utils/ApiUrl.js
@@ -1,24 +1,13 @@
 // src/constants.js or wherever this file is
 
-export const API_URL = typeof window !== 'undefined'
-  ? window.location.hostname === 'vrtualarena.ca'
-    ? 'http://vrtualarena.ca/api/v1'
-    : 'http://69.62.69.119:8080/api/v1'
-  : 'http://69.62.69.119:8080/api/v1';
+export const API_URL = 'http://69.62.69.119:8080/api/v1';
 
 // Helper function to get the base URL for media/assets
 export const getMediaBaseUrl = () => {
-  if (typeof window === 'undefined') return 'http://69.62.69.119:8080';
-  return window.location.hostname === 'vrtualarena.ca'
-    ? 'http://vrtualarena.ca'
-    : 'http://69.62.69.119:8080';
+  return 'http://69.62.69.119:8080';
 };
 
-export const SOCKET_URL = typeof window !== 'undefined'
-  ? window.location.hostname === 'vrtualarena.ca'
-    ? 'http://vrtualarena.ca:5000'
-    : 'http://69.62.69.119:5000'
-  : 'http://69.62.69.119:5000';
+export const SOCKET_URL = 'http://69.62.69.119:5000/';
 
 export const getAuthHeaders = () => {
   const authToken = localStorage.getItem('token');
