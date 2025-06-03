adadc2761cce59f3bc0101c43bea89bb9e525b78
Saad Shoaib <binshoaibsaad@gmail.com>
2025-06-03 22:53:42 +0500

    configuring base path

--- virtual-arena-admin/next.config.mjs
+++ virtual-arena-admin/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: '/admin',
+  assetPrefix: '/admin',
+}
 
-export default nextConfig;
+export default nextConfig
