86b15438d1329ae9113e47a880a2f43213c928fe
Saad Shoaib <binshoaibsaad@gmail.com>
2025-06-09 20:11:05 +0500

    ssl security (https) added

--- virtual-arena-user/src/utils/ApiUrl.js
+++ virtual-arena-user/src/utils/ApiUrl.js
@@ -1,13 +1,13 @@
 // src/constants.js or wherever this file is
 
-export const API_URL = 'http://69.62.69.119:8080/api/v1';
+export const API_URL = 'https://69.62.69.119:8080/api/v1';
 
 // Helper function to get the base URL for media/assets
 export const getMediaBaseUrl = () => {
-  return 'http://69.62.69.119:8080';
+  return 'https://69.62.69.119:8080';
 };
 
-export const SOCKET_URL = 'http://69.62.69.119:5000/';
+export const SOCKET_URL = 'https://69.62.69.119:5000/';
 
 export const getAuthHeaders = () => {
   const authToken = localStorage.getItem('token');
